fa32511edfa993d777f38299923cbb9921beb4c6
vishwas1 <vishwasanand.cse@gmail.com>
2024-01-23 17:23:27 +0530

    adding gh-pages

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/'
-    : '/',
-    chainWebpack: config => {
-      config.plugins.delete("prefetch");
-    },
+    : '/entity-developer-dashboard/',
+  chainWebpack: config => {
+    config.plugins.delete("prefetch");
+  },
   devServer: {
-      port: 9001
-    }
+    port: 9001
+  }
 }
