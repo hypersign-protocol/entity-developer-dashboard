27b7b993c81286d653a633af8cba923893594d23
vishwas1 <vishwasanand.cse@gmail.com>
2024-01-23 17:55:52 +0530

    added

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
+console.log(process.env.NODE_ENV)
+
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/'
+    ? '/entity-developer-dashboard/'
     : '/entity-developer-dashboard/',
   chainWebpack: config => {
     config.plugins.delete("prefetch");
