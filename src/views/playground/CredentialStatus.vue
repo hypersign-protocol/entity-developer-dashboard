c0bd63a5470ae7056668ac28114b92e85764e241
Varsha Kumari <varshakumari370@gmail.com>
2026-02-04 11:48:02 +0530

    update exploreor url

--- src/views/playground/CredentialStatus.vue
+++ src/views/playground/CredentialStatus.vue
@@ -316,7 +316,7 @@ h5 span {
               <!-- {{ row }} -->
               <td class="grabStyle">
                 <span @click="copyToClip(removeUrl(row.credentialMetadata.credentialId), 'Credential Id')">
-                   <a :href="`${$config.explorer.BASE_URL}/revocationRegistry/${row.id}`" target="_blank">{{
+                   <a :href="`${$config.explorer.BASE_URL}/identity/${row.id}`" target="_blank">{{
                   row.credentialMetadata.credentialId ? shorten(row.credentialMetadata.credentialId) : "-" }}</a>
                 </span>
               </td>
