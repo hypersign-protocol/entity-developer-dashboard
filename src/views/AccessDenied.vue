cc04cd8c5d5a415bca4555d92e5ff5818083f4a3
Pratap2018 <pratapmridha@gmail.com>
2026-05-29 12:20:31 +0530

    added top css

--- src/views/AccessDenied.vue
+++ src/views/AccessDenied.vue
@@ -1,3 +1,10 @@
 <template>
     <empty-container title="Access Denied" icon="fa fa-user-shield" class="mt-2"/>    
 </template>
+
+<style scoped>
+/* top space */
+.mt-2 {
+    margin-top: 20px;
+}
+</style>
